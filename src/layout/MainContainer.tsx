58a6366d7137ab915b9eb3b3394d372388e90483
swkoyo <10441291+swkoyo@users.noreply.github.com>
2022-11-01 17:39:47 +0900

    add padding to bottom

--- src/layout/MainContainer.tsx
+++ src/layout/MainContainer.tsx
@@ -6,7 +6,7 @@ export default function MainContainer({ children }: { children: ReactNode }) {
     return (
         <Box>
             <NavBar />
-            <Container maxW='container.lg' pt={20}>
+            <Container maxW='container.lg' pt={20} pb={10}>
                 {children}
             </Container>
         </Box>
