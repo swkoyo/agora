5246a4deef5385858608ccb3a39f03f2af662d56
swkoyo <10441291+swkoyo@users.noreply.github.com>
2022-11-04 18:43:01 +0900

    postclick redirect

--- src/features/post/PostList/index.tsx
+++ src/features/post/PostList/index.tsx
@@ -1,4 +1,5 @@
 import { Center, List, ListItem, Spinner, Text } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
 import { useEffectOnce } from 'usehooks-ts';
 import { useLazyGetPostsQuery } from '../../../api/post';
 import PostListItem from './PostListItem';
@@ -15,6 +16,7 @@ export default function PostList({
     showCommentForm?: boolean;
 }) {
     const [trigger, { data, isLoading, isFetching, isError }] = useLazyGetPostsQuery();
+    const navigate = useNavigate();
 
     useEffectOnce(() => {
         trigger({ topic_title: title });
@@ -44,10 +46,22 @@ export default function PostList({
         );
     }
 
+    const handlePostClick = (topicTitle: string, postId: number) => {
+        navigate(`/a/${topicTitle}/comments/${postId}`);
+    };
+
     return (
         <List w='full' spacing={4}>
             {data.data.map((d) => (
-                <ListItem key={d.id}>
+                <ListItem
+                    key={d.id}
+                    onClick={() => handlePostClick(d.topic.display_title, d.id)}
+                    sx={{
+                        _hover: {
+                            cursor: 'pointer'
+                        }
+                    }}
+                >
                     <PostListItem
                         post={d}
                         showTopic={showTopic}
