6bc6404fd64bbde28dd99c3553fdfec300424619
swkoyo <10441291+swkoyo@users.noreply.github.com>
2022-12-29 11:43:31 +0900

    fix post page

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,9 +1,8 @@
-import { Center, Container, Spinner, Text, useColorModeValue } from '@chakra-ui/react';
+import { Center, Spinner, Text } from '@chakra-ui/react';
 import { isFinite } from 'lodash';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useEffectOnce } from 'usehooks-ts';
 import { useLazyGetPostsQuery } from '../api/post';
-import PostHeader from '../features/post/PostHeader';
 import PostListItem from '../features/post/PostList/PostListItem';
 import TopicSidePanel from '../features/topic/TopicSidePanel';
 import useAuth from '../hooks/useAuth';
@@ -15,7 +14,6 @@ export default function Post() {
     const navigate = useNavigate();
     const [trigger, { data, isLoading, isFetching, isError }] = useLazyGetPostsQuery();
     const auth = useAuth();
-    const gridBackground = useColorModeValue('gray.700', 'rgb(3,3,3)');
 
     useEffectOnce(() => {
         if (!topic || !post_id) {
@@ -55,29 +53,23 @@ export default function Post() {
     }
 
     return (
-        <MainContainer maxW='full' background='gray.900' pb={0}>
-            <Container p={0} maxWidth='container.xl'>
-                <PostHeader post={data.data[0]} />
-                <GridContainer
-                    mainContent={
-                        <PostListItem
-                            post={data.data[0]}
-                            showCommentForm={!!auth}
-                            showComments
-                            showTopic
-                            hideJoin
-                            showFull
-                        />
-                    }
-                    sideContent={<TopicSidePanel topic={data.data[0].topic} showHeader buttonType='join' />}
-                    gap={4}
-                    sx={{
-                        px: 28,
-                        py: 8,
-                        background: gridBackground
-                    }}
-                />
-            </Container>
+        <MainContainer>
+            <GridContainer
+                mainContent={
+                    <PostListItem
+                        post={data.data[0]}
+                        showCommentForm={!!auth}
+                        showComments
+                        showTopic
+                        hideJoin
+                        showFull
+                    />
+                }
+                sideContent={<TopicSidePanel topic={data.data[0].topic} showHeader buttonType='join' />}
+                sx={{
+                    pt: 8
+                }}
+            />
         </MainContainer>
     );
 }
