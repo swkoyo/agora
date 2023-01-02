1482f6d00fb0ed5171ed47ba963ee4d8f5f0b16b
swkoyo <10441291+swkoyo@users.noreply.github.com>
2023-01-02 15:16:38 +0900

    use hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import theme from './config/theme';
 import ScrollToTop from './layout/ScrollToTop';
@@ -9,11 +9,11 @@ import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <ChakraProvider theme={theme}>
-        <BrowserRouter>
+        <HashRouter>
             <Provider store={store}>
                 <ScrollToTop />
                 <App />
             </Provider>
-        </BrowserRouter>
+        </HashRouter>
     </ChakraProvider>
 );
